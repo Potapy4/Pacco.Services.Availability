8b29747efce982d4014df81290c7ef93b61f78cc
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-01-29 11:56:28 +0100

    CustomerCreated

---
+++ src/Pacco.Services.Availability.Application/Events/External/Handlers/CustomerCreatedHandler.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using Convey.CQRS.Events;
+
+namespace Pacco.Services.Availability.Application.Events.External.Handlers
+{
+    public class CustomerCreatedHandler : IEventHandler<CustomerCreated>
+    {
+        public Task HandleAsync(CustomerCreated @event) => Task.CompletedTask;
+    }
+}
