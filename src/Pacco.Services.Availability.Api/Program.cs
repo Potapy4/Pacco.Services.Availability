7c80e5e08bee62abe628eb79772c8b6daaf90f86
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-01-29 07:16:13 +0100

    GetResources

--- src/Pacco.Services.Availability.Api/Program.cs
+++ src/Pacco.Services.Availability.Api/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Convey;
 using Convey.Configurations.Vault;
 using Convey.Logging;
@@ -40,6 +41,7 @@ namespace Pacco.Services.Availability.Api
                     .UseDispatcherEndpoints(endpoints => endpoints
                         .Get("", ctx => ctx.Response.WriteAsync(ctx.RequestServices.GetService<AppOptions>().Name))
                         .Get<GetResource, ResourceDto>("resources/{resourceId}")
+                        .Get<GetResources, IEnumerable<ResourceDto>>("resources")
                         .Post<AddResource>("resources", afterDispatch: (cmd, ctx)
                             => ctx.Response.Created($"resources/{cmd.ResourceId}"))
                         .Post<ReserveResource>("resources/{resourceId}/reservations/{dateTime}")))
