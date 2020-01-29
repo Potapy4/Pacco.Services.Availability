2be5b3e09230b0fdefc4585f41174eefdc649a58
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-01-29 15:14:04 +0100

    events

--- src/Pacco.Services.Availability.Infrastructure/Exceptions/ExceptionToMessageMapper.cs
+++ src/Pacco.Services.Availability.Infrastructure/Exceptions/ExceptionToMessageMapper.cs
@@ -1,5 +1,6 @@
 using System;
 using Convey.MessageBrokers.RabbitMQ;
+using Pacco.Services.Availability.Application.Commands;
 using Pacco.Services.Availability.Application.Events.Rejected;
 using Pacco.Services.Availability.Application.Exceptions;
 using Pacco.Services.Availability.Core.Exceptions;
@@ -9,11 +10,18 @@ namespace Pacco.Services.Availability.Infrastructure.Exceptions
     internal sealed class ExceptionToMessageMapper : IExceptionToMessageMapper
     {
         public object Map(Exception exception, object message)
-            => message switch
+            => exception switch
             {
                 ResourceAlreadyExistsException ex => new AddResourceRejected(ex.ResourceId, ex.Message, ex.Code),
                 MissingResourceTagsException ex => new AddResourceRejected(Guid.Empty, ex.Message, ex.Code),
                 InvalidResourceTagsException ex => new AddResourceRejected(Guid.Empty, ex.Message, ex.Code),
+                CannotExpropriateReservationException ex => new ReserveResourceRejected(ex.ResourceId, ex.Message,
+                    ex.Code),
+                ResourceNotFoundException ex => message switch
+                {
+                    ReserveResource command => new ReserveResourceRejected(command.ResourceId, ex.Message, ex.Code),
+                    _ => null
+                },
                 _ => null
             };
     }
